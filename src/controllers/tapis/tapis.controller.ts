3aa213da2d7f43811cc9070b8f7adfbd0ab2df87
Muhammad Rafly El Quraish <m.rafly@kuncicoin.com>
2023-12-19 22:38:57 +0700

    add new field in tapis

--- src/controllers/tapis/tapis.controller.ts
+++ src/controllers/tapis/tapis.controller.ts
@@ -216,9 +216,9 @@ export class TapisController {
 
     const predictions = (tfliteModel as any).predict(reshapedImage);
     const outputImage = predictions.squeeze();
-    const outputBuffer = await tf.node.encodePng(outputImage);
-    console.log(outputBuffer);
+    // const outputBuffer = await tf.node.encodePng(outputImage);
+    // console.log(outputBuffer);
 
-    return outputBuffer;
+    return outputImage;
   }
 }
